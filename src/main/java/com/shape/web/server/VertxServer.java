a0ed87f4627df351284d4e03e4c5766d043e1a3b
seongahjo <seongside@gmail.com>
2016-03-02 01:55:25 +0900

    JAVA 8 적용

--- src/main/java/com/shape/web/server/VertxServer.java
+++ src/main/java/com/shape/web/server/VertxServer.java
@@ -41,154 +41,136 @@ public class VertxServer extends DefaultEmbeddableVerticle {
         HttpServer server = vertx.createHttpServer(); //HTTP Server 생성
 
         io = new DefaultSocketIOServer(vertx, server);
-        io.sockets().onConnection(new Handler<SocketIOSocket>() {//Connection Event
-
-            public void handle(final SocketIOSocket socket) {
-                socket.on("join", new Handler<JsonObject>() { //room
-                    public void handle(JsonObject event) {
-                        String projectIdx = event.getString("projectIdx");
-                        ServerUser su = new ServerUser(projectIdx, event.getInteger("userIdx"), event.getString("userId"), event.getString("userName"), event.getString("userImg"), socket.getId());
-                        Clients.put(socket.getId(), su); // Socket에 해당하는 Room저장
-                        if (!projectIdx.equals("")) {
-                            logger.info("방 아이디 : " + projectIdx + " 접속 " + socket.getId());
-                            if (Rooms.get(projectIdx) != null)//방이 존재할경우
-                                logger.info("방 접속 ");
-                            else {
-                                //방이 존재하지 않을경우
-                                Rooms.put(projectIdx, -1);
-                                logger.info("방 생성");
-                            }
-                            socket.join(projectIdx);
-                            for (ServerUser temp : Clients.values())
-                                io.sockets().in(temp.getProjectIdx()).emit("adduser", temp.getId());
-                        }
+        io.sockets().onConnection(socket -> {
+            socket.on("join", event -> {
+                String projectIdx = event.getString("projectIdx");
+                ServerUser su = new ServerUser(projectIdx, event.getInteger("userIdx"), event.getString("userId"), event.getString("userName"), event.getString("userImg"), socket.getId());
+                Clients.put(socket.getId(), su); // Socket에 해당하는 Room저장
+                if (!projectIdx.equals("")) {
+                    logger.info("방 아이디 : " + projectIdx + " 접속 " + socket.getId());
+                    if (Rooms.get(projectIdx) != null)//방이 존재할경우
+                        logger.info("방 접속 ");
+                    else {
+                        //방이 존재하지 않을경우
+                        Rooms.put(projectIdx, -1);
+                        logger.info("방 생성");
                     }
-                }); //Join End
+                    socket.join(projectIdx);
+                    for (ServerUser temp : Clients.values())
+                        io.sockets().in(temp.getProjectIdx()).emit("adduser", temp.getId());
+                }
+            }); //Join End
 
 
-                socket.onDisconnect(new Handler<JsonObject>() {
-                    public void handle(JsonObject event) {
-                        boolean flag = true;
-                        ServerUser su = Clients.get(socket.getId());
-                        if (!su.getProjectIdx().equals("")) {
-                            if (su != null) {
-                                String projectIdx = su.getProjectIdx();
-                                logger.info("방나감 :: " + projectIdx + socket.getId());
-                                socket.leave(projectIdx);
+            socket.onDisconnect(event -> {
+                boolean flag = true;
+                ServerUser su = Clients.get(socket.getId());
+                if (!su.getProjectIdx().equals("")) {
+                    if (su != null) {
+                        String projectIdx = su.getProjectIdx();
+                        logger.info("방나감 :: " + projectIdx + socket.getId());
+                        socket.leave(projectIdx);
 
-                                for (ServerUser temp : Clients.values()) {   // 남아있는 Client들
-                                    //새로고침시 방 삭제 방지
-                                    logger.info(temp.getName() + " vs "+su.getName());
-                                    logger.info(temp.getProjectIdx()+" vs "+su.getProjectIdx());
-                                    if ((temp.getName().equals(su.getName())) && (temp.getProjectIdx().equals(su.getProjectIdx()))) {
-                                        flag = false;
-                                        break;
-                                    }
-                                }
-                                if (flag) {
-                                    logger.info("나감 : " + su.getName());
-                                    io.sockets().in(projectIdx).emit("deleteuser", su.getName());
-                                }
-                                if (Rooms.get(projectIdx) == (su.getUserIdx())) {
-                                    logger.info("writer 초기화");
-                                    Rooms.replace(projectIdx, -1);
-                                }
-                                if (io.sockets().clients(projectIdx) == null) {
-                                    logger.info("방삭제");
-                                    Rooms.remove(projectIdx);
-                                }
+                        for (ServerUser temp : Clients.values()) {   // 남아있는 Client들
+                            //새로고침시 방 삭제 방지
+                            logger.info(temp.getName() + " vs " + su.getName());
+                            logger.info(temp.getProjectIdx() + " vs " + su.getProjectIdx());
+                            if ((temp.getName().equals(su.getName())) && (temp.getProjectIdx().equals(su.getProjectIdx()))) {
+                                flag = false;
+                                break;
                             }
                         }
-                        Clients.remove(socket.getId());
-                    }
-                }); //Disconnect End
-
-
-                socket.on("msg", new Handler<JsonObject>() { // 메세지 전송
-                    public void handle(JsonObject event) {
-                        ServerUser su = Clients.get(socket.getId());
-                        String projectIdx = su.getProjectIdx();
-                        logger.info("메세지 : " + event.getString("msg"));
-                        event.putString("msg", CommonUtils.encodeContent(event.getString("msg")));
-                        event.putString("img", su.getImg());
-                        event.putString("user", su.getName());
-                        io.sockets().in(projectIdx).emit("response", event);
-                    }
-                }); //Msg End
-
-                socket.on("img", new Handler<JsonObject>() {
-                    public void handle(JsonObject event) {
-                        ServerUser su = Clients.get(socket.getId());
-                        String projectIdx = su.getProjectIdx();
-                        event.putString("img", su.getImg());
-                        event.putString("user", su.getName());
-                        event.putString("msg", "<img src=../loadImg?name=" + event.getString("msg") + " style=\'width:200px;height:150px\'>");
-                        io.sockets().in(projectIdx).emit("response", event);
-                    }
-                });//img end
-
-                socket.on("writer", new Handler<JsonObject>() {
-                    public void handle(JsonObject event) {
-                        ServerUser su = Clients.get(socket.getId());
-                        String projectIdx = su.getProjectIdx();
-                        Integer writer_id = su.getUserIdx();
-                        if (Rooms.get(projectIdx) != -1) {
-                            socket.emit("write", "no");
-                        } else {
-                            Rooms.replace(projectIdx, writer_id); //쓰는 사람의 id로 변경
-                            socket.emit("write", "yes");
-                            io.sockets().in(projectIdx).except(socket.getId()).emit("write", "no");
+                        if (flag) {
+                            logger.info("나감 : " + su.getName());
+                            io.sockets().in(projectIdx).emit("deleteuser", su.getName());
+                        }
+                        if (Rooms.get(projectIdx) == (su.getUserIdx())) {
+                            logger.info("writer 초기화");
+                            Rooms.replace(projectIdx, -1);
+                        }
+                        if (io.sockets().clients(projectIdx) == null) {
+                            logger.info("방삭제");
+                            Rooms.remove(projectIdx);
                         }
                     }
-                }); //writer end
+                }
+                Clients.remove(socket.getId());
 
-                socket.on("refreshToAll", new Handler<JsonObject>() {
-                    public void handle(JsonObject event) {
-                        ServerUser su = Clients.get(socket.getId());
-                        String memo = event.getString("memo");
-                        String projectIdx = su.getProjectIdx();
-                        io.sockets().in(projectIdx).emit("refresh", memo);
-                    }
-                });
-                socket.on("save", new Handler<JsonObject>() {
-                    public void handle(JsonObject event) {
-                        ServerUser su = Clients.get(socket.getId());
-                        String projectIdx = su.getProjectIdx();
-                        String memo = event.getString("memo");
-                        Project pj = pjs.get(Integer.parseInt(projectIdx));
-                        pj.setMinute(memo);
-                        Minute minute = ms.getByDate(new Date());
-                        if (minute == null) {
-                            minute = new Minute(memo, new Date());
-                            logger.info("새로 생성했다!");
-                        }
-                            minute.setContent(memo);
-                            minute.setDate(new Date());
-                            minute.setProject(pj);
-                        logger.info("메모장 : " + memo);
-                        ms.save(minute);
-                        pjs.save(pj);
-                        Rooms.replace(projectIdx, -1);
-                        try {
-                            FileUtil.MakeMinute(Integer.parseInt(projectIdx), memo);
-                        } catch (NumberFormatException e) {
-                            e.printStackTrace();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        io.sockets().in(projectIdx).emit("refresh", memo);
-                    }
-                }); //save end
+            }); //Disconnect End
 
-                socket.on("invite", new Handler<JsonObject>() {
-                    public void handle(JsonObject event) {
-                        Integer userIdx = Integer.parseInt(event.getString("userIdx"));
-                        for (ServerUser temp : Clients.values())
-                            if (temp.getUserIdx() == userIdx)
-                                io.sockets().socket(temp.getSocketId(), false).emit("alarm");
-                    }
-                });
-            }
+
+            socket.on("msg", event -> {
+                ServerUser su = Clients.get(socket.getId());
+                String projectIdx = su.getProjectIdx();
+                logger.info("메세지 : " + event.getString("msg"));
+                event.putString("msg", CommonUtils.encodeContent(event.getString("msg")));
+                event.putString("img", su.getImg());
+                event.putString("user", su.getName());
+                io.sockets().in(projectIdx).emit("response", event);
+            }); //Msg End
+
+            socket.on("img", event -> {
+                ServerUser su = Clients.get(socket.getId());
+                String projectIdx = su.getProjectIdx();
+                event.putString("img", su.getImg());
+                event.putString("user", su.getName());
+                event.putString("msg", "<img src=../loadImg?name=" + event.getString("msg") + " style=\'width:200px;height:150px\'>");
+                io.sockets().in(projectIdx).emit("response", event);
+            });//img end
+
+            socket.on("writer", event -> {
+                ServerUser su = Clients.get(socket.getId());
+                String projectIdx = su.getProjectIdx();
+                Integer writer_id = su.getUserIdx();
+                if (Rooms.get(projectIdx) != -1) {
+                    socket.emit("write", "no");
+                } else {
+                    Rooms.replace(projectIdx, writer_id); //쓰는 사람의 id로 변경
+                    socket.emit("write", "yes");
+                    io.sockets().in(projectIdx).except(socket.getId()).emit("write", "no");
+                }
+            }); //writer end
+
+            socket.on("refreshToAll", event -> {
+                ServerUser su = Clients.get(socket.getId());
+                String memo = event.getString("memo");
+                String projectIdx = su.getProjectIdx();
+                io.sockets().in(projectIdx).emit("refresh", memo);
+            });
+            socket.on("save", event -> {
+                ServerUser su = Clients.get(socket.getId());
+                String projectIdx = su.getProjectIdx();
+                String memo = event.getString("memo");
+                Project pj = pjs.get(Integer.parseInt(projectIdx));
+                pj.setMinute(memo);
+                Minute minute = ms.getByDate(new Date());
+                if (minute == null) {
+                    minute = new Minute(memo, new Date());
+                    logger.info("새로 생성했다!");
+                }
+                minute.setContent(memo);
+                minute.setDate(new Date());
+                minute.setProject(pj);
+                logger.info("메모장 : " + memo);
+                ms.save(minute);
+                pjs.save(pj);
+                Rooms.replace(projectIdx, -1);
+                try {
+                    FileUtil.MakeMinute(Integer.parseInt(projectIdx), memo);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                io.sockets().in(projectIdx).emit("refresh", memo);
+            }); //save end
+
+            socket.on("invite", event -> {
+                Integer userIdx = Integer.parseInt(event.getString("userIdx"));
+                for (ServerUser temp : Clients.values())
+                    if (temp.getUserIdx() == userIdx)
+                        io.sockets().socket(temp.getSocketId(), false).emit("alarm");
+            });
         });// onConnection end
         server.listen(port);
     }
