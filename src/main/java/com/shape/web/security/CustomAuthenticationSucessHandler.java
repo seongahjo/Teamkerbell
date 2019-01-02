41c7af6d30690a3fac1286b578151c56767e3217
seongahjo <seongside@gmail.com>
2019-01-02 18:48:18 +0900

    Filter: Clean Code

--- src/main/java/com/shape/web/security/CustomAuthenticationSucessHandler.java
+++ src/main/java/com/shape/web/security/CustomAuthenticationSucessHandler.java
@@ -3,8 +3,6 @@ package com.shape.web.security;
 import com.shape.web.entity.User;
 import com.shape.web.repository.UserRepository;
 import com.shape.web.service.LogsService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -15,40 +13,41 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.stream.StreamSupport;
 
 /**
  * Created by seongahjo on 2016. 7. 15..
  */
 public class CustomAuthenticationSucessHandler extends SavedRequestAwareAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-    private static final Logger logger = LoggerFactory.getLogger(CustomAuthenticationSucessHandler.class);
+
+    private UserRepository userRepository;
+
+    private LogsService logService;
+
 
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    LogsService logService;
-
+    public CustomAuthenticationSucessHandler(UserRepository userRepository, LogsService logService) {
+        this.userRepository = userRepository;
+        this.logService = logService;
+    }
 
     @Override
     public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException, ServletException {
         User u = userRepository.findById(authentication.getName());
-        request.getSession().setAttribute("useridx",u.getUseridx());
+        request.getSession().setAttribute("useridx", u.getUseridx());
 
-        String ip=request.getHeader("X_FORWARDED_FOR");
-        if(ip==null)
-            ip=request.getRemoteAddr();
+        String ip = request.getHeader("X_FORWARDED_FOR");
+        if (ip == null)
+            ip = request.getRemoteAddr();
         // 127.0.0.1 IPv4
         // -Djava.net.preferIPv4Stack=true
         // 0:0:0:0:0:0:0:1 IPv6
-        StringBuilder sb=new StringBuilder();
-        Enumeration<String> it= request.getHeaderNames();
-        while(it.hasMoreElements()){
-            String temp=it.nextElement();
-            sb.append(temp+":"+request.getHeader(temp)+"\\");
+        StringBuilder sb = new StringBuilder();
+        Enumeration<String> it = request.getHeaderNames();
+        while (it.hasMoreElements()) {
+            String temp = it.nextElement();
+            sb.append(temp + ":" + request.getHeader(temp) + "\\");
         }
-        logService.addLog(ip,sb.toString(),u);
-        handle(request,response,authentication);
+        logService.addLog(ip, sb.toString(), u);
+        handle(request, response, authentication);
     }
 }
