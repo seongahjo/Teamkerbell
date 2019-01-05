9a4b04c0832225dba2f4708a59d865dcbfc0b133
seongahjo <seongside@gmail.com>
2019-01-05 15:23:15 +0900

    Reduce: Clean Code

--- src/main/java/com/shape/web/parser/WordReduce.java
+++ src/main/java/com/shape/web/parser/WordReduce.java
@@ -1,92 +1,72 @@
 package com.shape.web.parser;
-import java.io.File;
-import java.util.ArrayList;
 
-public class WordReduce {
-    ArrayList<String> frontlist = new ArrayList<String>(); // 앞조사
-    ArrayList<String> endfirstlist = new ArrayList<String>(); //뒷앞조사
-    /*
-    ex) 있,했
-     */
-    ArrayList<String> endsecondlist = new ArrayList<String>();
-    //ex) 고, 을, 를
-    ArrayList<String> endthirdlist = new ArrayList<String>();
-    /*
-    기호
-     */
-    ArrayList<String> notlist = new ArrayList<String>(); //예외
+import java.util.*;
 
-    void defaultList() {
-        //
-        frontlist.add("\"");
-        //동사 앞쪽 형태소
-        endfirstlist.add("있");
-        endfirstlist.add("했");
-        //
-        endsecondlist.add("인");
-        endsecondlist.add("가");
-        endsecondlist.add("의");
-        endsecondlist.add("와");
-        endsecondlist.add("를");
-        endsecondlist.add("로");
-        endsecondlist.add("이");
-        endsecondlist.add("다");
-        endsecondlist.add("고");
-        endsecondlist.add("는");
-        endsecondlist.add("을");
-        endsecondlist.add("및");
-        endsecondlist.add("고");
-        endsecondlist.add("하고");
-        endsecondlist.add(",");
-        endsecondlist.add("?");
-        endsecondlist.add("에서");
-        endsecondlist.add("수");
-        //기호
-        endthirdlist.add(".");
-        endthirdlist.add(":");
-        endthirdlist.add("-");
-        endthirdlist.add("!");
-        endthirdlist.add("\"");
-        //
-        notlist.add("온라인");
+class WordReduce {
+
+    private static class Word implements Comparable<Word> {
+        private int priority;
+        private ArrayList<String> list;
+
+        Word(int priority, List<String> list) {
+            this.priority = priority;
+            this.list = new ArrayList<>(list);
+        }
+
+
+        public List<String> getList() {
+            return list;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Word word = (Word) o;
+            return priority == word.priority &&
+                    Objects.equals(list, word.list);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(priority, list);
+        }
+
+        @Override
+        public int compareTo(Word o) {
+            return Integer.compare(o.priority, priority);
+        }
     }
 
-    public WordReduce() {
-        defaultList();
+
+    private static ArrayList<Word> endList = new ArrayList<>();
+    private static ArrayList<String> startList = new ArrayList<>();
+    private static ArrayList<String> exceptList = new ArrayList<>(); //예외
+
+    static {
+        startList.add("\"");
+        endList.add(new Word(1, Arrays.asList("있", "했")));
+        endList.add(new Word(2, Arrays.asList("인", "가", "의", "와", "를", "로", "이", "다",
+                "고", "는", "을", "및", "하고", "고", ",", "?", "에서", "수")));
+        endList.add(new Word(3, Arrays.asList(".", ":", "-", "!", "\"")));
+        exceptList.add("온라인");
+        Collections.sort(endList);
     }
 
-    public WordReduce(File file) {
-        defaultList();
+    private WordReduce() {
+        /*
+         */
     }
 
-    public String simplify(String content) {
-        for (String temp : notlist) {
-            if (content.equals(temp))
-                return content;
-        }
-        for (String temp : frontlist) {
-            if (content.startsWith((temp))) {
-                content = content.substring(temp.length(), content.length());
-            }
-        }
-        for (String temp : endthirdlist) {
-            if (content.endsWith((temp))) {
-                content = content.substring(0, content.length() - temp.length());
-            }
-        }
-        for (String temp : endsecondlist) {
-            if (content.endsWith((temp))) {
-                content = content.substring(0, content.length() - temp.length());
-            }
-        }
-        for (String temp : endfirstlist) {
-            if (content.endsWith((temp))) {
-                content = content.substring(0, content.length() - temp.length());
-            }
-        }
 
+    static String simplify(String content) {
+        if (exceptList.stream().anyMatch(content::equals))
+            return content;
+        if (startList.stream().anyMatch(content::startsWith))
+            content = content.substring(1);
+        // 구조에 대해 생각해봐야할듯...
         if (content.equals(""))
-            return null;
+            return "";
         return content;
     }
 
