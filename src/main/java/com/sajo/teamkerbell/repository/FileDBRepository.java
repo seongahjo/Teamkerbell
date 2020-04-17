18d3a1930fd3620586b5ab46387916f075905caa
seongahjo <seongside@gmail.com>
2020-04-18 05:59:46 +0900

    Add find files, images in repository

--- src/main/java/com/sajo/teamkerbell/repository/FileDBRepository.java
+++ src/main/java/com/sajo/teamkerbell/repository/FileDBRepository.java
@@ -1,8 +1,11 @@
 package com.sajo.teamkerbell.repository;
 
 import com.sajo.teamkerbell.entity.FileDB;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by seongahjo on 2016. 7. 2..
@@ -10,4 +13,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FileDBRepository extends JpaRepository<FileDB, Integer> {
     FileDB findByStoredName(String storedName);
+
+    List<FileDB> findByProjectId(Integer projectId, Pageable pageable);
+
+    List<FileDB> findByProjectIdAndFileType(Integer projectId, FileDB.FileType fileType, Pageable pageable);
 }
